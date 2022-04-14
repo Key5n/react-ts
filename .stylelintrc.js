f8e55facbc4826de3be3644805d923b6fa08097f
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-04-14 18:36:19 +0900

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.eports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'string-quotes': 'single',
+  },
+};
